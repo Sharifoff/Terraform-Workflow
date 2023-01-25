2676f9bae148df64669cd89466f8f1f0574610d8
Sharifoff <efqanserifov@gmail.com>
2023-01-25 13:43:57 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_instance" "vm" {
-  ami           = "ami-0fe472d8a85bc7b0e"
-  subnet_id     = "subnet-0191b4917480b59db"
+  ami           = "ami-0b5eea76982371e91"
+  subnet_id     = "subnet-088826663bb5575b8"
   instance_type = "t3.micro"
   tags = {
     Name = "my-first-tf-node"
